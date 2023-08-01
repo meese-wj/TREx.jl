365271f3172fe4bc5749f58a8b14490047893d9e
Documenter.jl <documenter@juliadocs.github.io>
2023-08-01 19:15:17 +0000

    build based on f9b91c9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TREx","category":"page"},{"location":"#TREx","page":"Home","title":"TREx","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TREx.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TREx]","category":"page"}]
+}
