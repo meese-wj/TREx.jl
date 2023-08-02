0077862d3b3674f1f5215bc49ac65895333719a6
W. Joe Meese <meese022@umn.edu>
2023-08-02 17:30:24 -0500

    Add some more tests for Parameters

--- test/Parameters/test_Parameters.jl
+++ test/Parameters/test_Parameters.jl
@@ -10,19 +10,30 @@ using TREx
             TREx.thermalization_sweeps(p::TestSimParams) = 2
             TREx.measurement_sweeps(p::TestSimParams) = 4
 
-            @test TREx.thermalization_sweeps(TestSimParams()) == 2
-            @test TREx.measurement_sweeps(TestSimParams()) == 4
+            @test thermalization_sweeps(TestSimParams()) == 2
+            @test measurement_sweeps(TestSimParams()) == 4
 
             struct FakeSimParams <: Parameters.SimulationParameters end
 
-            @test_throws ArgumentError TREx.thermalization_sweeps(FakeSimParams())
-            @test_throws ArgumentError TREx.measurement_sweeps(FakeSimParams())
+            @test_throws ArgumentError thermalization_sweeps(FakeSimParams())
+            @test_throws ArgumentError measurement_sweeps(FakeSimParams())
 
             struct FakeModelParams <: Parameters.ModelParameters end
 
-            @test_throws MethodError TREx.thermalization_sweeps(FakeModelParams())
-            @test_throws MethodError TREx.measurement_sweeps(FakeModelParams())
+            @test_throws MethodError thermalization_sweeps(FakeModelParams())
+            @test_throws MethodError measurement_sweeps(FakeModelParams())
         end
+    end
+
+    @testset "MetropolisParameters" begin
+        p = Parameters.MetropolisParameters(; therm_sweeps = 24, mc_sweeps = 42)
+
+        @test thermalization_sweeps(p) == 24
+        @test measurement_sweeps(p) == 42
+
+        @test_throws MethodError Parameters.MetropolisParameters(; therm_sweeps = "hi")
+        @test_throws MethodError Parameters.MetropolisParameters(; mc_sweeps = "hi")
+        @test_throws MethodError Parameters.MetropolisParameters(; temperatures = "hi")
     end
 
 end
