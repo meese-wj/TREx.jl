22b9a7ee44918bbdb2138fc41c375a319392ee24
W. Joe Meese <meese022@umn.edu>
2023-08-03 10:04:23 -0500

    Clean up Models namespace

--- src/Models/Models.jl
+++ src/Models/Models.jl
@@ -1,15 +1,12 @@
 module Models
 
-# Use this to reexport the namespaces from each submodule
-using Reexport
-
 include("Lattices/Lattices.jl")
 export Lattices
 
 include("Hamiltonians/Hamiltonians.jl")
-@reexport using .Hamiltonians
+export Hamiltonians
 
 include("Observables/Observables.jl")
-@reexport using .Observables
+export Observables
 
 end
