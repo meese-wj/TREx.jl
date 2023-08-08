2ebb5f62854ec1c000f1d6f8e1afa95853bae45b
Documenter.jl <documenter@juliadocs.github.io>
2023-08-08 19:29:44 +0000

    build based on 765dbbd

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TREx","category":"page"},{"location":"#TREx.jl","page":"Home","title":"TREx.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TREx.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TREx]","category":"page"}]
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TRExMC","category":"page"},{"location":"#TRExMC.jl","page":"Home","title":"TRExMC.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TRExMC.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TRExMC]","category":"page"}]
 }
