430436c8c0c3fc1711731acaa48fee649d53e7a5
W. Joe Meese <meese022@umn.edu>
2023-08-12 12:30:27 -0500

    Finish basic Iterator tests

--- src/Models/Hamiltonians/HamiltonianIterators.jl
+++ src/Models/Hamiltonians/HamiltonianIterators.jl
@@ -11,25 +11,14 @@ Iterface for iteration through a given Hamiltonian.
 """
 abstract type AbstractHamiltonianIterator end
 hamiltonian(itr::AbstractHamiltonianIterator) = itr.ham
+num_DoF(itr::AbstractHamiltonianIterator) = (num_DoF ∘ hamiltonian)(itr)
 DoF(itr::AbstractHamiltonianIterator, location) = DoF(itr |> hamiltonian, location)  
-
-_iter_done(DoFstate::Tuple{<: AbstractHamiltonianIterator, Int}) = DoFstate[end] > num_DoF(DoFstate[begin] |> hamiltonian)
 
 Base.iterate(::itr_t, args...) where itr_t <: AbstractHamiltonianIterator = throw(ArgumentError("No iteration technique has been defined for $itr_t types."))
 
 struct IterateByLocation{H <: AbstractHamiltonian} <: AbstractHamiltonianIterator
     ham::H
 end
-
-function Base.iterate(itr::IterateByLocation, DoFstate = (DoF(itr, 1), 1))
-    next_location = DoFstate[end] + 1
-    next_DoFstate = (DoFstate[begin], next_location)
-    if _iter_done(next_DoFstate)
-        return nothing
-    end
-    return (DoFstate, next_location)
-end
-
 struct IterateAtRandom{RNG <: AbstractRNG, H <: AbstractHamiltonian} <: AbstractHamiltonianIterator
     rng::RNG
     ham::H
@@ -37,14 +26,25 @@ end
 
 IterateAtRandom(ham) = IterateAtRandom(Random.GLOBAL_RNG, ham)
 
-_random_location(itr) = rand(itr.rng, 1:num_DoF(itr |> hamiltonian))
+_random_location(itr) = rand(itr.rng, 1:num_DoF(itr))
 
-function Base.iterate(itr::IterateAtRandom, DoFstate = (DoF(itr, _random_location(itr)), 1) )
-    next_iteration = DoFstate[end] + 1
-    next_DoFstate = (DoFstate[begin], next_iteration)
-    if _iter_done(next_DoFstate)
+_iter_done(itr, current_itr) = current_itr > num_DoF(itr)
+
+function Base.iterate(itr::IterateByLocation, DoFstate = (DoF(itr, 1), 1))
+    current_itr = DoFstate[end]
+    if _iter_done(itr, current_itr)
         return nothing
     end
+    next_itr = current_itr + 1
+    return (DoF(itr, current_itr), next_itr)
+end
+
+function Base.iterate(itr::IterateAtRandom, DoFstate = (DoF(itr, _random_location(itr)), 1) )
+    current_itr = DoFstate[end]
+    if _iter_done(itr, current_itr)
+        return nothing
+    end
+    next_itr = current_itr + 1
     randloc = _random_location(itr)
-    return ( DoF(itr, randloc), next_iteration )
+    return ( DoF(itr, randloc), next_itr )
 end
--- test/Models/Hamiltonians/test_HamiltonianIterators.jl
+++ test/Models/Hamiltonians/test_HamiltonianIterators.jl
@@ -1,5 +1,6 @@
 using Test
 using TRExMC
+using Random 
 
 @info " ↳ Testing Iterators..."
 @testset "HamiltonianIterators.jl" begin
@@ -10,6 +11,55 @@ using TRExMC
         @test_throws ErrorException Hamiltonians.hamiltonian(FakeIter())
         @test_throws ErrorException Hamiltonians.DoF(FakeIter(), 1)
         @test_throws ArgumentError iterate(FakeIter())
+    end
+
+    @testset "Basic Iterator Usage" begin
+        
+        struct TestHamIter <: Hamiltonians.AbstractHamiltonian
+            values::Vector{Int}
+        end
+        Hamiltonians.DoF(ham::TestHamIter, loc) = ham.values[loc]
+        Hamiltonians.num_DoF(ham::TestHamIter) = length(ham.values)
+
+        vals = TestHamIter(1:5)
+        @test Hamiltonians.DoF(vals, 2) == 2
+        @test Hamiltonians.num_DoF(vals) == 5
+
+        @testset "IterateByLocation" begin
+            @test Hamiltonians.num_DoF(Hamiltonians.IterateByLocation(vals)) == Hamiltonians.num_DoF(vals)
+            @test all( Hamiltonians.iterate(Hamiltonians.IterateByLocation(vals)) .== (1, 2) ) 
+            @test Hamiltonians.iterate(Hamiltonians.IterateByLocation(vals), (5, 6)) isa Nothing 
+
+            iterated_vals = []
+            for iter in Hamiltonians.IterateByLocation(vals)
+                push!(iterated_vals, iter)
+            end
+            @test all(iterated_vals .== vals.values)
+        end
+        
+        @testset "IterateAtRandom" begin
+            rand_range = 1:1000
+            vals = TestHamIter(rand_range)
+            rng = Xoshiro(42)
+
+            @test Hamiltonians.num_DoF(Hamiltonians.IterateAtRandom(vals)) == Hamiltonians.num_DoF(vals)
+            @test Hamiltonians.iterate(Hamiltonians.IterateAtRandom(vals), (5, length(rand_range) + 1)) isa Nothing 
+
+            iterated_indices = []
+            iterated_values = []
+            for (iter, val) in enumerate( Hamiltonians.IterateAtRandom(rng, vals) )
+                push!(iterated_indices, iter)
+                push!(iterated_values, val)
+            end
+            unique_vals = unique(iterated_values)
+
+            @test length(iterated_indices) == Hamiltonians.num_DoF(vals)
+            @test length(iterated_values) == Hamiltonians.num_DoF(vals)
+            @test minimum(rand_range) ≤ minimum(unique_vals)
+            @test maximum(rand_range) ≥ maximum(unique_vals)
+            @test all(iterated_indices .== vals.values)
+        end
+
     end
 
 end
