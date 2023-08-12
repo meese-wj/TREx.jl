021e538b802de370f4a9d6ab1f69038948353303
Documenter.jl <documenter@juliadocs.github.io>
2023-08-12 18:17:44 +0000

    build based on 6e8766d

---
+++ v0.0.2/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TRExMC","category":"page"},{"location":"#TRExMC.jl","page":"Home","title":"TRExMC.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TRExMC.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TRExMC]","category":"page"}]
+}
