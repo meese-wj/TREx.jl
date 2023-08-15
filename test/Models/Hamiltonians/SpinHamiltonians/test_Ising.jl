d39cdf7491d01729c0f97dd30b70b72328f5a232
W. Joe Meese <meese022@umn.edu>
2023-08-14 21:10:48 -0500

    Fix test deps

--- test/Models/Hamiltonians/SpinHamiltonians/test_Ising.jl
+++ test/Models/Hamiltonians/SpinHamiltonians/test_Ising.jl
@@ -22,15 +22,18 @@ using BenchmarkTools
             params = Hamiltonians.BasicIsingParameters(1.0)
             latt = Lattices.CubicLattice2D(4, 4)
             ham = Hamiltonians.BasicIsingHamiltonian(latt, params)
-
+            
             @test Hamiltonians.num_DoF(ham) == Lattices.num_sites(latt)
             @test Hamiltonians.DoF(ham, 1) isa Hamiltonians.SpinHamiltonians.IsingDoF
             @test Hamiltonians.DoF(ham, 1) |> Hamiltonians.location isa Int
             @test Hamiltonians.DoF(ham, 1) |> Hamiltonians.location == 1
             @test Hamiltonians.DoF(ham, 1) |> Hamiltonians.value isa typeof(params.Jex)
         end
-
+        
         @testset "Iteration" begin
+            params = Hamiltonians.BasicIsingParameters(1.0)
+            latt = Lattices.CubicLattice2D(4, 4)
+            ham = Hamiltonians.BasicIsingHamiltonian(latt, params)
             
             bm = @benchmark iterate( Hamiltonians.IterateByLocation($ham) )
             @test bm.allocs == 0
