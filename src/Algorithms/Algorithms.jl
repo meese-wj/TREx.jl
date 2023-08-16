9f3269946c85ae580c8033b5031293da12c20533
W. Joe Meese <meese022@umn.edu>
2023-08-16 12:52:06 -0500

    Add a procedure method to see what the algorithm macro does

--- src/Algorithms/Algorithms.jl
+++ src/Algorithms/Algorithms.jl
@@ -3,9 +3,10 @@ module Algorithms
 using ..TRExMC
 import ..TRExMC: Model # import and export this just to make the name available
 
-export Model, @algorithm
+export Model, procedure, @algorithm
 
 abstract type AbstractAlgorithm end
+procedure(::alg_t, args...) where alg_t <: AbstractAlgorithm = throw(ArgumentError("No procedure has been defined for $alg_t types.")) 
 
 """
     @algorithm
@@ -24,7 +25,26 @@ function _check_func(name, idx, func::F) where F
     func isa Function ? nothing : throw(ArgumentError("@algorithm $name argument $idx, $func::$(func |> typeof), is not a function."))
 end
 
-function _algorithm(export_alg, name, fxns...)    
+# TODO: Is it worth fixing this?
+# function _check_for_Algorithms()
+#     check_Algs = quote
+#         :TRExMC ∈ names(Main; imported = true) ? nothing : throw(ErrorException("Somehow you managed to call @algorithm without importing TRExMC. This is impressive."))
+#         # :Algorithms ∈ names(Main; imported = true) ? nothing : throw(ErrorException("Somehow you managed to call @algorithm without importing Algorithms. This is impressive."))
+#     end
+#     return check_Algs |> Base.remove_linenums!
+# end
+
+function _define_procedure(export_alg, name, fxns...)
+    expr = Expr(:block)
+    expr.args = [:( import .Algorithms: procedure )]
+    push!(expr.args, :( procedure(::$(Symbol(name))) = Algorithms._algorithm_setup($export_alg, $name, $(fxns...)) |> Base.remove_linenums! ) )
+    if export_alg
+        push!(expr.args, :(export procedure))
+    end
+    return expr |> Base.remove_linenums!
+end
+
+function _algorithm_setup(export_alg, name, fxns...)    
     name = Symbol(name)
     str_expr = :( struct $name <: Algorithms.AbstractAlgorithm end )
     body_expr = Expr(:block)
@@ -44,6 +64,15 @@ function _algorithm(export_alg, name, fxns...)
     if export_alg
         push!(expr.args, :(export $name))
     end
+    return expr |> Base.remove_linenums!
+end
+
+function _algorithm(export_alg, name, fxns...)
+    expr = Expr(:block)
+    # expr = _check_for_Algorithms()
+    append!(expr.args, _algorithm_setup(export_alg, name, fxns...).args)
+    append!(expr.args, _define_procedure(export_alg, name, fxns...).args)
+    push!(expr.args, :(methods($(Symbol(name)))))
     return expr |> Base.remove_linenums!
 end
 
