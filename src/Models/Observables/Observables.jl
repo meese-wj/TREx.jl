790dc9cc12a9ffd82826fb448df3b883f54d5bc2
W. Joe Meese <meese022@umn.edu>
2023-08-21 16:45:01 -0500

    Start writing the observables macro

--- src/Models/Observables/Observables.jl
+++ src/Models/Observables/Observables.jl
@@ -1,6 +1,6 @@
 module Observables
 
-export AbstractObservables, measure!
+export AbstractObservables, measure!, @observables
 
 """
     abstract type AbstractObservables end
@@ -24,5 +24,19 @@ Empty observable used for testing purposes.
 """
 struct NullObservable <: AbstractObservables end
 measure!(obs::NullObservable, args...) = nothing 
+
+"""
+    @observables name symbols funcs
+
+Generate an [`AbstractObservables`](@ref) subtype with the given `name`
+whose fields will be denoted by the given `symbols` iterable. The [`measure!`](@ref)
+method will also be implemented with the supplied [`funcs`] iterable.
+
+!!! note
+    `length(symbols) == length(funcs)` by construction.
+"""
+macro observables(name, symbols, funcs)
+    _observables(name, symbols, funcs)
+end
 
 end # Observables
