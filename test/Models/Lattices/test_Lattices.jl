99f1d3e0ff2f0d33b48516c23196f5536800e29f
W. Joe Meese <meese022@umn.edu>
2023-08-22 21:51:41 -0500

    Add tests for AbstractLattices

--- test/Models/Lattices/test_Lattices.jl
+++ test/Models/Lattices/test_Lattices.jl
@@ -3,5 +3,16 @@ using TRExMC
 
 @info "↳ Testing Lattices.jl"
 @testset "Lattices.jl" begin
+
+    @testset "AbstractLattice" begin
+        struct FakeLattice <: Lattices.AbstractLattice end
+
+        @test_throws MethodError Lattices.construct_lattice!(FakeLattice())
+        @test_throws MethodError Lattices.num_sites(FakeLattice())
+        @test_throws MethodError Lattices.site_index(FakeLattice(), 1)
+        @test_throws MethodError Lattices.nearest_neighbors(FakeLattice(), 1)
+        @test_throws MethodError Lattices.parameters(FakeLattice())
+    end
+
     include("test_CubicLattices.jl")
 end
