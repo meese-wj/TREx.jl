5078f5348586aeb2301eb684905e3501c4233e22
W. Joe Meese <meese022@umn.edu>
2024-08-02 12:46:58 -0500

    Remove show debuggers

--- src/Algorithms/Canonical/Canonical.jl
+++ src/Algorithms/Canonical/Canonical.jl
@@ -2,9 +2,6 @@ module Canonical
 
 using ..Algorithms
 import .Algorithms.Models
-# import ..Algorithms: iterate_DoFs, DoF_location, DoF_value, current_state, proposed_state, DoF_energy_change
-@show names(Algorithms)
-@show names(Models)
 
 include("Metropolis.jl")
 
